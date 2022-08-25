6d6ab1be2e5438440af647a293ff526b81d1b552
TGSAN <h@iloli.tv>
2022-08-25 22:07:05 +0800

    First version

---
+++ Internet Explorer Launcher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InternetExplorerLauncher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type IEType = Type.GetTypeFromProgID("InternetExplorer.Application");
+            dynamic IEInst = Activator.CreateInstance(IEType);
+            IEInst.Visible = true;
+            if (args.Length > 0)
+            {
+                string URL = args[0];
+                IEInst.Navigate(ref URL);
+            }
+            else 
+            {
+                IEInst.GoHome();
+            }
+        }
+    }
+}
